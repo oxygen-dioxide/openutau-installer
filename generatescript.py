2f3586d51cf5bd27feb9ed43f43e4f23fbf5978f
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2022-07-27 08:00:31 +0800

    Update generatescript.py

--- generatescript.py
+++ generatescript.py
@@ -1,7 +1,8 @@
 import os
 import sys
 (_,filepath,arch,version)=sys.argv
+xarch={"x86":"x86", "x64":"amd64"}[arch]
 with open(os.path.join(filepath,"template.nsi"),"r",encoding="utf8") as inputfile:
-    outputstr=inputfile.read().replace("{arch}",arch).replace("{version}",version)
+    outputstr=inputfile.read().replace("{arch}",arch).replace("{version}",version).replace("{xarch}",xarch)
 with open(os.path.join(filepath,arch+".nsi"),"w",encoding="utf8") as outputfile:
     outputfile.write(outputstr)
